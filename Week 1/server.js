927d1b81d9b2a7e2c4a1fd9e90f3e1e2c0e3e5dc
ShreyasNair934 <shreyasnair934@gmail.com>
2024-03-24 17:37:58 +1100

    comment update

--- Week 1/server.js
+++ Week 1/server.js
@@ -3,18 +3,21 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+//create a route
 app.get('/addTwoNumbers', (req, res) => {
-    // Get the query parameters from the URL
+    
+    //Get two numbers from the query string in the URL
     const number1 = parseInt(req.query.num1);
     const number2 = parseInt(req.query.num2);
 
-    // Perform the addition
+   //store the result of adding the two numbers
     const result = number1 + number2;
 
-    // Send the result as the response
+    //send a response to the client
     res.json({statuscode: 200, data: `The result of ${number1} + ${number2} is ${result}`});
 });
 
+//start the server
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
